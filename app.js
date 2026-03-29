e5150f6dc6f6c0ee3f66c0e5b98befe9dbbc6ac7
kiranmai2005 <vsskiranmai2005@gmail.com>
2026-03-29 11:31:56 +0530

    CRUD done

--- app.js
+++ app.js
@@ -48,6 +48,17 @@ app.get('/campgrounds/:id/edit',async(req,res)=>{
     const campground=await Campground.findById(req.params.id)
     res.render('campgrounds/edit',{campground});
 })
+app.put('/campgrounds/:id',async(req,res)=>{
+    const {id}=req.params;
+    const campground=await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id',async (req,res)=>{
+    const {id}=req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
