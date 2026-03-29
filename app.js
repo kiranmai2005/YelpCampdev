824c63309c94f117806895c1515d200a246e0263
kiranmai2005 <vsskiranmai2005@gmail.com>
2026-03-29 11:22:58 +0530

    some changes and added editroute

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ db.once("open",()=>{
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -33,11 +34,20 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
+app.post('/campgrounds',async(req,res)=>{
+    const campground=res.send(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 });
 
+app.get('/campgrounds/:id/edit',async(req,res)=>{
+    const campground=await Campground.findById(req.params.id)
+    res.render('campgrounds/edit',{campground});
+})
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
